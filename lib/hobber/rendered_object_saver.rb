a3c00eead2bf9e463dd265b043fba5492fba6edb
Boy Maas <boy.maas@gmail.com>
2012-06-25 21:02:00 +0200

    rendered object saver

--- lib/hobber/rendered_object_saver.rb
+++ lib/hobber/rendered_object_saver.rb
@@ -4,13 +4,22 @@ require 'fileutils'
 module Hobber
   class RenderedObjectSaver
     include FileUtils
+    include Thor::Shell
+
     def initialize(rendered_object)
       @rendered_object = rendered_object 
     end
 
     def save
-      path_name = Pathname.new( @rendered_object.path )
-      # mkdir_p path_name.dirname
+      path_name = Pathname.new(@rendered_object.path)
+      mkdir_p(path_name.dirname)
+      unless file_collision(path_name)
+        return
+      end
+
+      File.open(@rendered_object.path, 'w') do |f|
+        f.write(@rendered_object.data)
+      end
     end
   end
 end
